f9b49a108408c69d523155f878e1d32f52ef5eba
rodijanssen <74348802+rodijanssen@users.noreply.github.com>
2023-01-13 08:50:40 +0100

    sommenkiezen.php

--- Functions/main_functions.php
+++ Functions/main_functions.php
@@ -46,7 +46,7 @@ class User
 
     public function randomPassword()
     {
-        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()';
         $pass = array(); //remember to declare $pass as an array
         $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
         for ($i = 0; $i < 8; $i++) {
---
+++ Pages/sommenkiezen.php
@@ -0,0 +1,24 @@
+<?php
+include '../Functions/main_functions.php';
+include '../Assets/layout.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sommenkiezen</title>
+</head>
+
+<body>
+    <div class="container h-100 d-flex align-items-center justify-content-center" style="margin-top: 300px;">
+        <a type="button" href="?+" class="btn btn-outline-primary btn-info" style="margin: 10px;">+</a>
+        <a type="button" href="?-" class="btn btn-outline-primary btn-info" style="margin: 10px;">-</a>
+        <a type="button" href="?*" class="btn btn-outline-primary btn-info" style="margin: 10px;">*</a>
+        <a type="button" href="?/" class="btn btn-outline-primary btn-info" style="margin: 10px;">/</a>
+    </div>
+</body>
+
+</html>
